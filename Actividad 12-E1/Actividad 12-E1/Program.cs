414e0368626e63cc8f458a13980b23a924171347
erick <erick@LEGI0SORIO>
2026-02-22 11:46:59 -0600

    Proyecto creado

--- Actividad 12-E1/Actividad 12-E1/Program.cs
+++ Actividad 12-E1/Actividad 12-E1/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Prioridad médica de un paciente: ");
+﻿Console.WriteLine("MENU");
+Console.WriteLine("1. Emergencia");
+Console.WriteLine("2. Consulta");
+Console.WriteLine("3. Pediatría");
+Console.WriteLine("4. Traumatología");
+Console.Write("Seleccionar una opción: ");
+int opcion = int.Parse(Console.ReadLine());
