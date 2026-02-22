62df2f8c380ab23184e2d25c1819539c9b5c59d8
erick <erick@LEGI0SORIO>
2026-02-22 11:57:21 -0600

    “Switch implementado”

--- Actividad 12-E1/Actividad 12-E1/Program.cs
+++ Actividad 12-E1/Actividad 12-E1/Program.cs
@@ -5,3 +5,27 @@ Console.WriteLine("3. Pediatría");
 Console.WriteLine("4. Traumatología");
 Console.Write("Seleccionar una opción: ");
 int opcion = int.Parse(Console.ReadLine());
+switch (opcion)
+{
+    case 1:
+        {
+            break;
+        }
+    case 2:
+        {
+            break;
+        }
+    case 3:
+        {
+            break;
+        }
+    case 4:
+        {
+            break;
+        }
+    default:
+        {
+            Console.WriteLine("Opción no válida");
+            break;
+        }
+}
